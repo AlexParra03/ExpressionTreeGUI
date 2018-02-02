2d7e29a9ae63d52263a81233bbe1aabf81de0858
David Alejandro Parra Moreno <alex.user03@gmail.com>
2018-02-02 17:30:01 -0500

    Added libGDX engine

---
+++ desktop/src/com/gui/expression_tree/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.gui.expression_tree.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.gui.expression_tree.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
